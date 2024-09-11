bef40edd86751c2b56883cdfa82e4fe209e3797f
Hossein <hossein.gasemzadeh03@gmail.com>
2024-09-11 16:43:26 +0330

    chore: organize project structure and prepare packaging
    - Restructured project directories for better organization
    
    - Created setup.py for packaging the application
    
    - Ensured all necessary files are included for distribution

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup( 
+	name='pycrp', 
+	version='1', 
+	description='A Python package for securely encrypting and decrypting files with advanced algorithms.', 
+	author='Hossein gasemzadeh', 
+	author_email='hossein.gasemzadeh03@gmail.com', 
+    packages=find_packages(), 
+ 
+    install_requires=[ 
+        'cryptography==42.0.8',
+        'termcolor==2.4.0',
+	], 
+    
+
+) 
