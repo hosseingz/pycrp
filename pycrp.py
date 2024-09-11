2fee4e6a85786be19c4574829e33bdbdbda59ac7
Hossein <hossein.gasemzadeh03@gmail.com>
2024-09-11 07:46:25 +0330

    feat: add primitive methods

--- pycrp.py
+++ pycrp.py
@@ -1,1 +1,27 @@
-print('Hello World !')
+from cryptography.fernet import Fernet
+from base64 import b64encode
+from hashlib import sha256
+
+
+class Crp:
+    def __init__(self, key:str):
+        
+        if not key:
+            raise ValueError("An empty key is not Valid input.")
+        
+        hash = sha256(key.encode()).digest()
+        key = b64encode(hash)
+        
+        self.__f = Fernet(key)
+        
+
+    def encrypt(self, data:bytes) -> bytes:
+        if not data:
+            raise ValueError('Data cannot be empty.')
+        return self.__f.encrypt(data)
+    
+    def decrypt(self, data:bytes) -> bytes:
+        if not data:
+            raise ValueError('Data cannot be empty.')
+        return self.__f.decrypt(data)
+        
