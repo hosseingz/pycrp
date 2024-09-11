36ce825d7c5203f31f6d57b1ea8eb004ff256af9
Hossein <hossein.gasemzadeh03@gmail.com>
2024-09-11 09:31:21 +0330

    feat: Implement load_file and dump_crp functions for file encryption
    - Added the load_file function to load a file's content and prepare it for encryption.
    - Implemented the dump_crp function to save encrypted data to a .crp file in a specified directory, ensuring the directory exists before saving.
    - Enhanced error handling to manage invalid file paths and empty data inputs, improving overall robustness.
    - Utilized the pickle module for serializing the data before encryption.
    
    These changes enable users to easily load files and store their encrypted versions, streamlining the encryption workflow.

--- pycrp.py
+++ pycrp.py
@@ -1,7 +1,10 @@
 from cryptography.fernet import Fernet
 from base64 import b64encode
 from hashlib import sha256
+from termcolor import colored
 
+import os
+import pickle
 
 class Crp:
     def __init__(self, key:str):
@@ -24,4 +27,47 @@ class Crp:
         if not data:
             raise ValueError('Data cannot be empty.')
         return self.__f.decrypt(data)
+    
+    
+    def load_file(self, path:str):
+        if not path:
+            raise ValueError("File path cannot be empty.")
         
+        elif not os.path.isfile(path):
+            raise FileNotFoundError('The file does not exist.')
+        
+        
+        with open(path, 'rb') as file:
+            self.__crp = {
+                'file_name': os.path.basename(path),
+                'data': file.read(),
+            }
+    
+    def dump_crp(self, file_name:str=None, export_dir_path:str=None):
+        if hasattr(self, '__crp'):
+            if file_name:
+                baseName, currentEx = os.path.splitext(file_name)
+                
+                if currentEx != '.crp':
+                    file_name = f'{baseName}.crp'
+
+            if not (export_dir_path and os.path.isdir(export_dir_path)):
+                print(colored(f'This Dir does not exist !. File will be saved in "crp-files/"', 'red'))
+                export_dir_path = self.__ensure_dir_exists('crp-files/')
+                
+                
+            path = os.path.join(export_dir_path, file_name)
+            data = self.encrypt( pickle.dumps(self.__crp) )
+            
+            with open(path, 'wb') as file:
+                file.write(data)
+        else:
+            raise ValueError('Encrypt a file before dumping it.')
+    
+    
+    def __ensure_dir_exists(self, path:str):
+        
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        return path
